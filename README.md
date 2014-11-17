6d7c53d818a90521dce98bbad983ed0d93258c85
Jonathan Seligsohn <jbseligsohn@gmail.com>
2014-11-17 14:31:07 -0500

    Updated based on initial feedback

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-How to play: Use arrow keys on your keyboard to move the player. Avoid hitting bugs or falling into the water.
+To install:
+Download the repository (https://github.com/JSeligsohn/frontend-nanodegree-arcade-game). Open up index.html in your browser and play!
+
+How to play: 
+Use arrow keys on your keyboard to move the player. Avoid hitting bugs or falling into the water.
 Good luck!
--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
     this.x = Math.floor(Math.random() * 150);
     this.y = Math.floor(Math.random() * 200 + 30);
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -17,12 +17,15 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += 30*dt;
-}
+    if (this.x > 520) {
+        this.x = -1000;
+    };
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -31,15 +34,15 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 400;
-}
+};
 
 Player.prototype.update = function() {
     checkCollision(this);
-}
+};
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 
 Player.prototype.handleInput = function(key) {
@@ -55,39 +58,39 @@ Player.prototype.handleInput = function(key) {
     else if (key == 'up') {
         this.y -= 30;
     }
-}
+};
 
 checkCollision = function(player) {
     //check boundaries
     if (player.x > 420) {
         player.x = 420;
-        console.log('Out of bounds');
+        console.log('Out of bounds'); //Out of bounds
     }
     else if (player.x < -20) {
         player.x = -10;
-        console.log('Out of bounds');
+        console.log('Out of bounds'); //Out of bounds
     }
     else if (player.y > 435) {
         player.y = 435  ;
-        console.log('Out of bounds');
+        console.log('Out of bounds'); //Out of bounds
     }
     else if (player.y < 15) {
         player.x = 200;
         player.y = 400;
-        console.log('Hit water!');
+        console.log('Hit water!'); //Hit Water
     }
     //check enemies
     allEnemies.forEach(function(enemy) {
-        if ((enemy.y - 30) < player.y & 
-            player.y < (enemy.y + 30) & 
+        if ((enemy.y - 60) < player.y & 
+            player.y < (enemy.y + 60) & 
             (enemy.x - 60) < player.x & 
             player.x < (enemy.x + 60)) {
                 player.x = 200;
                 player.y = 400;
-                console.log('Hit enemy!');
-        }
-    })
-}
+                console.log('Hit enemy!'); //Collision with enemy
+        };
+    });
+};
 
 
 // Now instantiate your objects.
@@ -95,11 +98,11 @@ checkCollision = function(player) {
 // Place the player object in a variable called player
 allEnemies = [new Enemy()];
 
-for(i=0; i < 30; i++) {
+for(i=0; i < 15; i++) {
     newEnemy = new Enemy();
     newEnemy.x = newEnemy.x - i*150;
     allEnemies.push(newEnemy);
-}
+};
 
 player = new Player();
 
